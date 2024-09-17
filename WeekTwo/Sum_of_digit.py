ead87f61fcc1e362ca5c9565878f3fb60e5d3b6e
Julie0418 <khatoonjulekha4@gmail.com>
2024-09-17 19:05:55 -0400

    added sum_of_digit

---
+++ WeekTwo/Sum_of_digit.py
@@ -0,0 +1,15 @@
+
+user_input = int(input ('enter any number: '))
+sum_total = 0
+
+while user_input > 0:
+    last_digit = user_input % 10
+    sum_total += last_digit
+    user_input = user_input // 10
+
+print(sum_total)
+
+
+    
+
+
