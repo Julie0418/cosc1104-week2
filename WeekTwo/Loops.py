9497895dc246ef0d2d644ebb223488b086b5d5bf
Julie0418 <khatoonjulekha4@gmail.com>
2024-09-17 17:25:42 -0400

    added ex 1

--- WeekTwo/Loops.py
+++ WeekTwo/Loops.py
@@ -1,1 +1,23 @@
-print("hello World")
+
+# exercise 1 - vowel counter
+
+user_string = input('Enter any text: ').lower()
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+counters = [0, 0, 0, 0, 0]
+
+for word in user_string:
+    if word in vowels:
+        index = vowels.index(word)
+        counters[index] += 1
+    
+for i in range(len(vowels)):
+    print(f'{ vowels[i] } - { counters[i] }')
+
+
+
+# Sum of digit
+
+
+
